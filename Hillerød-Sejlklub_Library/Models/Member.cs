b5609e570ee14b1b808ce6a9b0643becceb44076
KingMohamad1 <detsejedrenge@gmail.com>
2025-12-02 09:53:07 +0100

    Implemented Class Member

---
+++ Hillerød-Sejlklub_Library/Models/Member.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hillerød_Sejlklub_Library.Models
+{
+    internal class Member
+    {
+        public string Name { get; set; }
+        public Member(string name) {
+            Name = name;
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name}";
+        }
+    }
+}
