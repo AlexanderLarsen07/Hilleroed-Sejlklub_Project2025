79ad48c133a3ee5b90090ed3a07a2486db62ab99
Alexander Larsen <all005@edu.zealand.dk>
2025-12-02 09:31:11 +0100

    Add project files.

---
+++ Hillerød-Sejlklub_Library/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Hillerød_Sejlklub_Library
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Hillerød-Sejlklub_Library/Models/Boat.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hillerød_Sejlklub_Library.Models
+{
+    public class Boat
+    {
+        public  int Id { get; set; }
+    }
+}
