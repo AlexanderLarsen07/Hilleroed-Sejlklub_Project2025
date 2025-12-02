557caf7b16f15b6e3461d35b2b306f90565e1818
Alexander Larsen <all005@edu.zealand.dk>
2025-12-02 14:25:05 +0100

    -added FuelTypeEnum, MærkeEnum og RoleEnum

---
+++ Hillerød-Sejlklub_Library/Models/FuelTypeEnum.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hillerød_Sejlklub_Library.Models
+{
+    public enum FuelTypeEnum { Diesel, Benzin, El}
+}
