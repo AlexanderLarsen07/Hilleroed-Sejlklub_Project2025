ffc498b5a425b0289ac30ed41328106fa2289e4c
Aksel Heilmann Møgelmose <akm002@edu.zealand.dk>
2025-12-02 10:19:45 +0100

    booking class test

--- Hillerød-Sejlklub_Library/Models/Boat.cs
+++ Hillerød-Sejlklub_Library/Models/Boat.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,5 +12,10 @@ namespace Hillerød_Sejlklub_Library.Models
     public class Boat
     {
         public  int Id { get; set; }
+        public void Print()
+        {
+
+        }
+
     }
 }
---
+++ Hillerød-Sejlklub_Library/Models/Booking.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hillerød_Sejlklub_Library.Models
+{
+    public class Booking
+    {
+        public string Destination { get; private set; }
+
+            
+        public bool Overdue { get; }
+        public override string ToString()
+        {
+            return $"{}";
+        }
+    }
+}
+
+   
