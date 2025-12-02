e709fe3ca5a8adf4e0f8e4fd6ba847b6876c89bd
Aksel Heilmann Møgelmose <akm002@edu.zealand.dk>
2025-12-02 11:43:23 +0100

    update i booking

--- Hillerød-Sejlklub_Library/Models/Booking.cs
+++ Hillerød-Sejlklub_Library/Models/Booking.cs
@@ -8,13 +8,30 @@ namespace Hillerød_Sejlklub_Library.Models
 {
     public class Booking
     {
+        private static int _bookingID = 0;
+
+        public int BookingID { get; private set; }
         public string Destination { get; private set; }
 
-            
+        public DateTime Start { get; private set; }
+
+        public DateTime End { get; private set; }
         public bool Overdue { get; }
+
+        public Booking(string destination, DateTime start, DateTime end, bool overdue)
+        {
+            _bookingID++;
+            BookingID = _bookingID;
+            Destination = destination;
+            Start = start;
+            End = end;
+            Overdue = overdue;
+            
+        }
+
         public override string ToString()
         {
-            return $"{}";
+            return $"Destination: {Destination}\n Start: {Start}\n End: {End}\n Overdue: {Overdue}";
         }
     }
 }
