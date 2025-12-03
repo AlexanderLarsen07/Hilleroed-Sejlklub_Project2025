411df106062d72e404e5bc114e0ab8a95f02c145
Aksel Heilmann Møgelmose <akm002@edu.zealand.dk>
2025-12-03 14:06:54 +0100

    New Update methods

---
+++ Hillerød-Sejlklub_Library/BookingRepo.cs
@@ -0,0 +1,49 @@
+﻿using Hillerød_Sejlklub_Library.Interfaces;
+using Hillerød_Sejlklub_Library.Models;
+
+namespace Hillerød_Sejlklub_Library
+{
+    public class BookingRepo : IBookingRepo
+    {
+        private List<Booking> _bookings;
+
+        public BookingRepo()
+        {
+            _bookings = new List<Booking>();
+        }
+        public void AddBooking(Booking booking)
+        {
+            foreach (Booking bookingOnList in _bookings)
+            {
+                if (booking.BookingID == bookingOnList.BookingID)
+                {
+                    throw new Exception("BookingID already exist");
+                }
+                
+            }
+            _bookings.Add(booking);
+        }
+
+        public Booking GetBookingByID(int BookingID)
+        {
+            for(int i = 0; _bookings.Count > i; i++)
+            {
+                if(BookingID == _bookings[i].BookingID)
+                {
+                    return _bookings[i];
+                }
+            }
+            throw new Exception("BookingID doesn’t exist");
+        }
+
+        public void PrintAllBookings()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveBookingByID(int BookingID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
