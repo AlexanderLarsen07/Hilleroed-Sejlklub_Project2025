92615c81f26496be109fd89c4d99ce6afae1ed82
KingMohamad1 <detsejedrenge@gmail.com>
2025-12-04 14:39:57 +0100

    d

--- Hillerød-Sejlklub_Library/Models/Members/BoatLot.cs
+++ Hillerød-Sejlklub_Library/Models/Members/BoatLot.cs
@@ -6,20 +6,23 @@ using System.Threading.Tasks;
 
 namespace Hillerød_Sejlklub_Library.Models.Members
 {
-    public class BoatLot
+    public class BoatLot //Oplysninger om båden
     {
-        private int _lotID;
-        public int LotID { get; }
+        private static int _lotID = 1;
+        public int LotID { get { return _lotID; } private set { value = _lotID; } }
         public int Length { get; }
         public int Width { get; }
         public BoatLot(int length, int width)
         {
             Length = length;
             Width = width;
+            LotID = _lotID++;
         }
-        public override string ToString()
+        public override string ToString() //implement those above
         {
-            return $"";
+            return $"ID: {LotID}\n" +
+                $"Length:{Length}cm.\n" +
+                $"Bredden:{Width}cm.";
         }
     }
 }
